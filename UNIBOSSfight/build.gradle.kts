aa0f7bdaad32aca11154e0695a451152439be951
Giovanni Prete <g.prete@cosmobile.com>
2023-02-21 15:47:46 +0100

    reset progetto

---
+++ UNIBOSSfight/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+
+val lwjglVersion = "3.1.3"
+
+val lwjglNatives = Pair(
+        System.getProperty("os.name")!!,
+        System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "FreeBSD", "SunOS", "Unit").any { name.startsWith(it) } ->
+            "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) }                ->
+            "natives-macos"
+        arrayOf("Windows").any { name.startsWith(it) }                           ->
+            "natives-windows"
+        else -> throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually")
+    }
+}
+
+dependencies {
+    implementation("org.lwjgl", "lwjgl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-assimp", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-glfw", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-nfd", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-openal", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-opengl", lwjglVersion)
+    implementation("org.lwjgl", "lwjgl-stb", lwjglVersion)
+    runtimeOnly("org.lwjgl", "lwjgl", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-assimp", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-glfw", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-nfd", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-openal", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", lwjglVersion, classifier = lwjglNatives)
+    runtimeOnly("org.lwjgl", "lwjgl-stb", lwjglVersion, classifier = lwjglNatives)
+}
