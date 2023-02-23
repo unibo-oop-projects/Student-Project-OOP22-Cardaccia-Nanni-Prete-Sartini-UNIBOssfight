6da775c46ca610bbadf1218ccc2ff58dc8270138
Giovanni Prete <g.prete@cosmobile.com>
2023-02-24 00:36:06 +0100

    aggiunti all'interfaccia Entity i metodi isDisplayed e render, che prende come argomento il contesto grafico in cui disegnare i personaggi.
    Il renderer prende come input contesto grafico, colori, posizione e dimensioni per disegnare l'entity

--- UNIBOSSfight/src/main/java/core/entity/Entity.java
+++ UNIBOSSfight/src/main/java/core/entity/Entity.java
@@ -1,9 +1,14 @@
 package core.entity;
 
 import core.component.Component;
+import javafx.scene.canvas.GraphicsContext;
 import util.Vector2d;
 
 public interface Entity {
+
+    boolean isDisplayed();
+
+    void render(GraphicsContext gc);
 
     void update();
 
