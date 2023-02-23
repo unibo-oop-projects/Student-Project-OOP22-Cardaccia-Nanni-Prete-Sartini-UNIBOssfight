b7a9574b95af5887ba8a6f6e3853971a2aa03029
Giovanni Prete <g.prete@cosmobile.com>
2023-02-23 17:19:03 +0100

    added javafx library

--- UNIBOSSfight/build.gradle.kts
+++ UNIBOSSfight/build.gradle.kts
@@ -1,15 +1,23 @@
 plugins {
     java
     application
-    id("java")
-
+    id("org.openjfx.javafxplugin") version "0.0.13"
 }
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
+}
+
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set(project.properties["mainClass"].toString())
 }
 
 dependencies {
