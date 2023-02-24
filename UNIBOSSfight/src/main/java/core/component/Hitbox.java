512a4394e9cb2834ff305693de8c234ab3c9adac
Matteo Sartini <matteo.sartini8@studio.unibo.it>
2023-02-24 11:50:35 +0100

    Corretto if statement in Hitbox.collide

--- UNIBOSSfight/src/main/java/core/component/Hitbox.java
+++ UNIBOSSfight/src/main/java/core/component/Hitbox.java
@@ -9,7 +9,7 @@ public abstract class Hitbox implements Component {
     private Vector2d position;
     private double leftSide, rightSide, topSide, bottomSide;
 
-    public Hitbox(int latOffset, int height, Vector2d startingPosition){
+    public Hitbox(double latOffset, double height, Vector2d startingPosition){
         this.lateralOffset = latOffset;
         this.height = height;
         this.position = startingPosition;
@@ -18,13 +18,8 @@ public abstract class Hitbox implements Component {
     }
 
     boolean collide(Hitbox target){
-
-        //X axis collisions
-        if(this.rightSide >= target.leftSide && this.leftSide <= target.rightSide && this.bottomSide <= target.topSide && this.topSide >= target.bottomSide){
-            return  true;
-        }
-
-        return false;
+        //X an Y axis collisions
+        return this.rightSide >= target.leftSide && this.leftSide <= target.rightSide && this.bottomSide <= target.topSide && this.topSide >= target.bottomSide;
     }
 
     public void update(Vector2d newPos){
