822462a552ba3cd97ebd97bfdc0623ddf7c53163
Matteo Sartini <matteo.sartini8@studio.unibo.it>
2023-02-27 18:18:50 +0100

    Implementato render() in WeaponImpl

--- UNIBOSSfight/src/main/java/impl/component/WeaponImpl.java
+++ UNIBOSSfight/src/main/java/impl/component/WeaponImpl.java
@@ -1,19 +1,27 @@
 package impl.component;
 
+import core.component.Renderer;
 import core.component.Transform;
 import core.component.Weapon;
 import core.entity.Bullet;
 import core.entity.Entity;
+import javafx.scene.canvas.GraphicsContext;
 
 public class WeaponImpl implements Weapon {
 
     private int damage;
     private Entity user;
+    private Renderer renderer;
 
-    public WeaponImpl(Entity user, int damage){
+    public WeaponImpl(Entity user, int damage, Renderer renderer){
         //TODO
         this.user = user;
         this.damage = damage;
+        this.renderer = renderer;
+    }
+
+    void render(GraphicsContext gc){
+        this.renderer.render(gc, this.user.getPosition().add(0, 0));
     }
 
     @Override
