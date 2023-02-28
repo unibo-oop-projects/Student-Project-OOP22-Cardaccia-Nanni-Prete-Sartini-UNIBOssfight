a613d2b4dccf190c675f76bb33dbf356e05a8ec2
Matteo Sartini <matteo.sartini8@studio.unibo.it>
2023-02-28 17:40:43 +0100

    Aggiornata Bullet

--- UNIBOSSfight/src/main/java/core/entity/Bullet.java
+++ UNIBOSSfight/src/main/java/core/entity/Bullet.java
@@ -1,7 +1,59 @@
 package core.entity;
 
-public interface Bullet extends Entity {
+import core.component.Hitbox;
+import core.component.Renderer;
+import core.component.Transform;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
 
-    int getDamage();
+public abstract class Bullet extends AbstractEntity {
+    private final double RIGHT_ANGLE = 90;
+    private final int SPEED = 10;
+
+    private int damage;
+    private Point2D target;
+    private  double angle;
+
+    public Bullet(Transform startingPos, int height, int width, Renderer renderer, int damage, Point2D target){
+
+        super(startingPos, height, width, renderer);
+
+        this.damage = damage;
+        this.target = target;
+        this.angle = startingPos.getPosition().angle(target);
+    }
+
+    @Override
+    public boolean isDisplayed(Point2D position) {
+        return false;
+    }
+
+    @Override
+    public void render(GraphicsContext gc, Point2D position) {
+
+    }
+
+    @Override
+    public void update(Inputs input) {
+
+        double deltaX, deltaY;
+        double oppositeAngleX = RIGHT_ANGLE - angle;
+        double oppositeAngleY = RIGHT_ANGLE - (RIGHT_ANGLE - angle);
+
+        deltaX = SPEED * Math.cos(oppositeAngleX);
+        deltaY = SPEED * Math.cos(oppositeAngleY);
+
+        position.move((int)deltaX, (int)deltaY);
+    }
+
+    @Override
+    public Point2D getPosition() {
+        return this.position.getPosition();
+    }
+
+    @Override
+    public Hitbox getHitbox() {
+        return this.hitbox;
+    }
 
 }
--- UNIBOSSfight/src/main/java/impl/entity/BulletImpl.java
+++ UNIBOSSfight/src/main/java/impl/entity/BulletImpl.java
@@ -1,69 +1,16 @@
 package impl.entity;
 
 import core.component.Hitbox;
+import core.component.Renderer;
 import core.component.Transform;
 import core.entity.Bullet;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.geometry.Point2D;
 import java.lang.Math;
 
-public class BulletImpl implements Bullet {
+public class BulletImpl extends Bullet {
 
-    private final double RIGHT_ANGLE = 90;
-
-    private int damage;
-    private  int speed;
-    private double angle;
-    private Transform position;
-    private Transform startingPosition;
-    private Transform target;
-    private Hitbox hitbox;
-
-    public BulletImpl(Transform startingPos, Transform target, int damage, int speed, double angle, Hitbox hitbox){
-        this.startingPosition = startingPos;
-        this.position = startingPos;
-        this.target = target;
-        this.damage = damage;
-        this.speed = speed;
-        this.angle = angle;
-        this.hitbox = hitbox;
-    }
-
-    @Override
-    public int getDamage() {
-        return this.damage;
-    }
-
-    @Override
-    public boolean isDisplayed(Point2D position) {
-        return false;
-    }
-
-    @Override
-    public void render(GraphicsContext gc, Point2D position) {
-
-    }
-
-    @Override
-    public void update(Inputs input) {
-
-        double deltaX, deltaY;
-        double oppositeAngleX = RIGHT_ANGLE - angle;
-        double oppositeAngleY = RIGHT_ANGLE - (RIGHT_ANGLE - angle);
-
-        deltaX = this.speed * Math.cos(oppositeAngleX);
-        deltaY = this.speed * Math.cos(oppositeAngleY);
-
-        position.move((int)deltaX, (int)deltaY);
-    }
-
-    @Override
-    public Point2D getPosition() {
-        return this.position.getPosition();
-    }
-
-    @Override
-    public Hitbox getHitbox() {
-        return this.hitbox;
+    public BulletImpl(Transform startingPos, int height, int width, Renderer renderer, int damage, Point2D target) {
+        super(startingPos, height, width, renderer, damage, target);
     }
 }
