20c81a946491f71649867ae49396031466cf7a5b
Denise Nanni <denise.nanni@studio.unibo.it>
2023-03-05 15:42:09 +0100

    aggiunta javadoc nel Collider e nel ColliderImpl

--- UNIBOSSfight/src/main/java/core/component/Collider.java
+++ UNIBOSSfight/src/main/java/core/component/Collider.java
@@ -6,8 +6,15 @@ import impl.entity.*;
 
 import java.util.function.Consumer;
 
+/**
+ * Interfaccia che gestisce i comportamenti delle entità alle collisioni con
+ * le varie entità.
+ */
 public interface Collider {
 
+    /**
+     * Enum con tutte le entità possibili
+     */
     enum Entities {
         // TODO valutare se mettere questa enum in un file a parte
         TMPENTITY(TmpEntityImpl.class),
@@ -25,12 +32,28 @@ public interface Collider {
             this.type = type;
         }
 
+        /**
+         * Verifica se il parametro è dello stesso tipo runtime di quello
+         * associato al valore dell'enum
+         * @param e entità da confrontare
+         * @param <T> sottotipo di entity
+         * @return true se il parametro è dello stesso tipo, false altrimenti
+         */
         public <T extends Entity> boolean equals(T e) {
             return e.getClass().equals(type);
         }
     }
 
+    /**
+     * Esegue la procedura di gestione della collisione
+     * @param e entità con cui il chiamante collide
+     */
     void manageCollision(Entity e);
 
+    /**
+     * Associa un comportamento ad una determinata entità
+     * @param key enum dell'entità
+     * @param value comportamento
+     */
     void addBehaviour(Entities key, Consumer<Entity> value);
 }
--- UNIBOSSfight/src/main/java/impl/component/ColliderImpl.java
+++ UNIBOSSfight/src/main/java/impl/component/ColliderImpl.java
@@ -7,19 +7,32 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
+
+/**
+ * {@inheritDoc}
+ */
 public class ColliderImpl implements Collider {
 
     private final Map<Entities, Consumer<Entity>> behaviours;
 
+    /**
+     * Crea una nuova istanza del gestore delle collisioni
+     */
     public ColliderImpl() {
         this.behaviours = new EnumMap<>(Entities.class);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void manageCollision(Entity e) {
         this.behaviours.entrySet().stream().filter(b -> b.getKey().equals(e))
                 .findFirst().ifPresent(b -> b.getValue().accept(e));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     public void addBehaviour(Entities key, Consumer<Entity> value) {
         this.behaviours.putIfAbsent(key, value);
     }
