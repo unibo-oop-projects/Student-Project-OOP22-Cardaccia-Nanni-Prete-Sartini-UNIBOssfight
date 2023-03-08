df7850c1ca4484d1295efaf51e3644f10b681f05
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-03-08 18:01:21 +0100

    Aggiunto metodo collect nel Coin

--- UNIBOSSfight/src/main/java/impl/entity/Coin.java
+++ UNIBOSSfight/src/main/java/impl/entity/Coin.java
@@ -12,9 +12,10 @@ import javafx.scene.paint.Color;
  */
 public abstract class Coin extends AbstractEntity {
     private final int value;
+    private boolean collected;
 
     /**
-     * The constructor of the class which creates a new instance of Coin.
+     * Creates a new instance of the class Coin.
      * @param position the position of the coin
      * @param height the height of the coin
      * @param width the width of the coin
@@ -25,13 +26,20 @@ public abstract class Coin extends AbstractEntity {
         super(position, height, width,
                 new SpriteRenderer(height, width, Color.YELLOW, filename));
         this.value = 1;
+        this.collected = false;
     }
 
     /**
+     * This method is used to collect the coin and get its value,
+     * that will be added to the player's score.
      * @return the value of the coin
      */
-    public int getValue() {
-        return this.value;
+    public int collect() {
+        if (!this.collected) {
+            this.collected = true;
+            return this.value;
+        }
+        return 0;
     }
 
     /**
@@ -39,7 +47,10 @@ public abstract class Coin extends AbstractEntity {
      */
     @Override
     public boolean isDisplayed(final Point2D position) {
-        return false;
+        if (collected) {
+            return false;
+        }
+        return super.isDisplayed(position);
     }
 
     /**
