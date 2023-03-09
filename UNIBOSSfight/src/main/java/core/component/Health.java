1d02a54a5386c21f42faf358fec403aef57d5ada
Denise Nanni <denise.nanni@studio.unibo.it>
2023-03-09 10:45:29 +0100

    javadoc in Health

--- UNIBOSSfight/src/main/java/core/component/Health.java
+++ UNIBOSSfight/src/main/java/core/component/Health.java
@@ -1,12 +1,15 @@
 package core.component;
 
 /**
- *
+ * This class models the health component which determines if an entity is dead or not.
  */
 public class Health implements Component {
 
     private int hp;
 
+    /**
+     * Creates a new instance of health.
+     */
     public Health() {
         this.hp = 100;
     }
@@ -20,10 +23,18 @@ public class Health implements Component {
         return this.hp;
     }
 
+    /**
+     * Subtracts the specified damage from the health.
+     * @param damage the damage received
+     */
     public void damage(final int damage) {
         this.hp -= damage;
     }
 
+    /**
+     * Determines if the entity is dead.
+     * @return if the entity is dead or not
+     */
     public boolean isDead() {
         return this.hp <= 0;
     }
