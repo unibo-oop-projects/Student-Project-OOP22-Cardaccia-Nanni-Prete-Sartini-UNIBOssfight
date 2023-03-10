4175159a4e8cf7ee18a294f6c892d6792b7d9e08
Livia Cardaccia <livia.cardaccia@studio.unibo.it>
2023-03-10 12:40:40 +0100

    Ultimate le javadoc delle implementazioni dei Component

--- UNIBOSSfight/src/main/java/impl/component/WeaponImpl.java
+++ UNIBOSSfight/src/main/java/impl/component/WeaponImpl.java
@@ -7,29 +7,44 @@ import core.entity.Bullet;
 import impl.entity.BulletImpl;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
-import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import util.Window;
 
+/**
+ * This class implements the Weapon.
+ */
 public class WeaponImpl implements Weapon {
 
     private final int damage;
     private final Transform userPos;
     private final Renderer renderer;
 
-    public WeaponImpl(final Transform userPos, final int damage, final Renderer renderer) {
-        //TODO
+    /**
+     * Creates a new instance of the class Weapon.
+     * @param userPos the position of the entity
+     *                which the weapon will be given to
+     * @param damage the damage that the weapon can cause
+     * @param renderer the renderer of the weapon
+     */
+    public WeaponImpl(final Transform userPos,
+                      final int damage, final Renderer renderer) {
         this.userPos = userPos;
         this.damage = damage;
         this.renderer = renderer;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Node render(final int direction, final int rotation) {
         return this.renderer.render(new Point2D(Window.getWidth() / 2 + 10 * direction,
                 this.userPos.getPosition().getY() + 80 - 110), direction, rotation);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Bullet fire(final Point2D target) {
 
