a9de0138a381e0f7f6681693b36ff737ac4dc4cd
Giovanni Prete <giovanni.prete@studio.unibo.it>
2023-03-20 10:04:53 +0100

    aggiunta bozza di deserializzatore

--- UNIBOSSfight/src/main/java/game/Prova.java
+++ UNIBOSSfight/src/main/java/game/Prova.java
@@ -1,8 +1,12 @@
 package game;
 
-import core.component.Transform;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import core.component.Renderer;
+import core.entity.AbstractEntity;
 import core.entity.Entity;
-import impl.entity.TmpEntityImpl;
+import impl.entity.PlayerImpl;
 import impl.level.LevelImpl;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -20,10 +24,19 @@ import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import util.AbstractEntityDeserializer;
+import util.PlayerImplDeserializer;
+import util.RendererDeserializer;
 import util.Window;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Prova extends Application {
 
@@ -32,12 +45,18 @@ public class Prova extends Application {
     private static final int MIN_WINDOW_HEIGHT = 600;
     private static final int MIN_WINDOW_WIDTH = 800;
 
-    private final LevelImpl currentLevel = new LevelImpl();
+    private final LevelImpl currentLevel = loadLevel();
     private Group root = new Group();
     private Scene currentScene;
     private InputManager inputManager;
     private Image image;
     private Paint imagePattern;
+
+    public static String readFile(String path, Charset encoding) throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
+    }
 
     @Override
     public void start(final Stage stage) {
@@ -49,7 +68,7 @@ public class Prova extends Application {
 
         FileInputStream input = null;
         try {
-            input = new FileInputStream("assets/ground2.png");
+            input = new FileInputStream("assets/ground/ground.png");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
@@ -83,7 +102,6 @@ public class Prova extends Application {
 
         currentScene.setOnMouseClicked(e -> this.currentLevel.playerShoot(new Point2D(e.getX(), e.getY())));
 
-
         //Adding scene to the stage
         stage.setScene(currentScene);
 
@@ -95,17 +113,23 @@ public class Prova extends Application {
         }));
         tl.setCycleCount(Animation.INDEFINITE);
 
-        this.currentLevel.addEntity(
-                new TmpEntityImpl(
-                        new Transform(new Point2D(500, Window.getHeight()), 0),
-                        50,
-                        50,
-                        "goomba.png"
-                )
-        );
+        try {
+            String json = readFile("output.json", StandardCharsets.UTF_8);
+            System.out.println("\n\n\n\nciaone");
+            GsonBuilder gsonBuilder = new GsonBuilder();
+
+            JsonDeserializer<PlayerImpl> deserializer = new PlayerImplDeserializer(); // implementation detail
+            gsonBuilder.registerTypeAdapter(PlayerImpl.class, deserializer);
+
+            Gson customGson = gsonBuilder.create();
+            LevelImpl customObject = customGson.fromJson(json, LevelImpl.class);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 
         tl.play();
         this.currentLevel.init();
+        System.out.println("questo livello ha questo numero di entità " + this.currentLevel.getEntities().size());
     }
 
     private void inputPoll() {
@@ -130,7 +154,7 @@ public class Prova extends Application {
     private void render() {
         this.root.getChildren().clear();
         root.getChildren().add(this.currentLevel.renderPlayer());
-        //root.getChildren().add(this.currentLevel.renderWeapon());
+        root.getChildren().add(this.currentLevel.renderWeapon());
         this.currentLevel.renderEntities().forEach(e -> root.getChildren().add(e));
 
         // create a Rectangle
@@ -220,4 +244,51 @@ public class Prova extends Application {
             return this.isSpacePressed;
         }
     }
+
+    @Override
+    public void stop() throws Exception {
+        try {
+            String jsonString = new GsonBuilder()
+                    //.excludeFieldsWithoutExposeAnnotation()
+                    .setPrettyPrinting()
+                    .create()
+                    .toJson(this.currentLevel);
+
+            FileWriter file = new FileWriter("output.json");
+            file.write(jsonString);
+            file.close();
+            System.out.println(jsonString);} catch (Exception e) {
+            e.printStackTrace();
+        }
+        super.stop();
+    }
+
+    private LevelImpl loadLevel() {
+        try {
+            String json = readFile("output.json", StandardCharsets.UTF_8);
+
+            GsonBuilder gsonBuilder = new GsonBuilder();
+
+            JsonDeserializer<PlayerImpl> deserializer = new PlayerImplDeserializer(); // implementation detail
+            JsonDeserializer<AbstractEntity> Edeserializer = new AbstractEntityDeserializer(); // implementation detail
+            JsonDeserializer<Renderer> Rdeserializer = new RendererDeserializer(); // implementation detail
+
+
+            gsonBuilder.registerTypeAdapter(Entity.class, Edeserializer);
+            gsonBuilder.registerTypeAdapter(Renderer.class, Rdeserializer);
+            gsonBuilder.registerTypeAdapter(PlayerImpl.class, deserializer);
+
+            Gson customGson = gsonBuilder.create();
+            LevelImpl customObject = customGson.fromJson(json, LevelImpl.class);
+            System.out.println(customObject.getPlayer().getPosition());
+            System.out.println("entities " + customObject.getPlayer().getBullets());
+
+            return customObject;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
 }
